6abee17e6b129888a32b53d3e74770ebf9805a33
Cyprian Żurawski <czurawski@creatuity.com>
2022-02-18 09:15:34 +0100

    styled loginPage

--- frontend/src/pages/loginPage/LoginPage.js
+++ frontend/src/pages/loginPage/LoginPage.js
@@ -1,49 +1,84 @@
 import React from "react";
-import './LoginPage.css'
-import { Button,
-    ButtonGroup,
-    TextField,
-    Typography
-} from "@mui/material";
+import { useState } from "react";
+import "./LoginPage.css";
+import { Button, ButtonGroup, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-   return <div className="login-wrapper">
-       <Typography variant="h1" component="div" >Login</Typography>
-       <form className="login-form">
-            <TextField
-                id="outlined-basic" 
-                label="User Name" 
-                variant="outlined" 
-                margin="normal"
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
-                />
-            <TextField 
-                id="outlined-basic" 
-                label="Password" 
-                variant="outlined"
-                margin="normal"
-                type="password" />
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-            <Box 
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                marginTop: '20px'
-            }}>
+    const loginData = { userName, userPassword };
 
-            <ButtonGroup variant="text" aria-label="text button group">
-                <Button type='submit'>Login</Button>
-                <Button onClick={()=>{
-                    window.location = '/register';
-                }} >Register</Button>
-            </ButtonGroup>
-            </Box>
-       </form>
-   </div>
-   
+    console.log(loginData);
+  };
+
+  return (
+    <div className="login-wrapper">
+      <Typography variant="h1" component="div">
+        Login
+      </Typography>
+
+      <form onSubmit={handleSubmit} className="login-form">
+        <TextField
+          id="outlined-basic"
+          label="User Name"
+          variant="outlined"
+          margin="normal"
+          autoComplete="off"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          margin="normal"
+          type="password"
+          onChange={(e) => setUserPassword(e.target.value)}
+        />
+
+        <Box
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: "20px",
+          }}
+        >
+          <Button
+            variant="contained"
+            type="submit"
+            style={{
+              width: "45%",
+            }}
+          >
+            Login
+          </Button>
+          <Button
+            style={{
+              width: "45%",
+            }}
+          >
+            <Link
+              to="/register"
+              style={{
+                textDecoration: "none",
+                color: "inherit",
+              }}
+            >
+              register
+            </Link>
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
