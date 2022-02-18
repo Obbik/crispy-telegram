b8b1a9c2fc5d797b0797de3d7d7a50a6c343d93e
Katarzyna Mirowska <kmirowska@creatuity.com>
2022-02-18 12:55:57 +0100

    Added register mutation

--- frontend/src/pages/RegisterPage/RegisterPage.js
+++ frontend/src/pages/RegisterPage/RegisterPage.js
@@ -1,34 +1,61 @@
 import React from "react";
 import { useState } from "react";
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
+import { Typography, Button, TextField, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { Link } from "react-router-dom";
 import './styles.css';
+import axios from "axios";
 
 const RegisterPage = () => {
-    const [name, setName ] = useState('')
-    const [surname, setSurname ] = useState('')
-    const [email, setEmail ] = useState('')
-    const [height, setHeight ] = useState('Midget')
+    const [ state, setState ] = useState({
+        name: '',
+        email: '',
+        password: '',
+        height: 'Midget'
+    })
     const [isPending, setIsPending] = useState(false)
+
+    let handleChange = (name, value) => {
+        setState({
+            ...state,
+            [name]: value
+        })
+        console.log(name, value)
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const registerData = { name, surname, email, height }
+        const { name, email, password, height } = state;
 
         setIsPending(true)
 
-        console.log(registerData)
+        axios({
+            url: 'http://localhost:3001/',
+            method: 'post',
+            data: {
+                query: `mutation  {
+                  createUser (name: "${name}",email: "${email}", password: "${password}", height: "${height}"){
+                    name,
+                    email,
+                    password,
+                    height
+                  }
+                }`
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+
+        console.log(name, email, password, height)
     }
 
     return <div className="register-page">
         <Typography variant="h1">
-            Yo, please register
+            Please register
         </Typography>
         <form
             onSubmit={handleSubmit}
@@ -36,35 +63,38 @@ const RegisterPage = () => {
             <div className="name-surname-wrapper">
                 <TextField
                     label="Name"
+                    name="name"
                     variant="outlined"
                     sx={{
                         width: '50%',
                         marginRight: '5px'
                     }}
-                    onChange={(e) => setName(e.target.value)}
+                    onChange={(e) => {handleChange(e.target.name, e.target.value)}}
                 />
                 <TextField
-                    label="Surname"
+                    label="E-mail"
+                    name="email"
                     variant="outlined"
                     sx={{
                         width: '50%',
                         marginLeft: '5px'
                     }}
-                    onChange={(e) => setSurname(e.target.value)}
+                    onChange={(e) => {handleChange(e.target.name, e.target.value)}}
                 />
             </div>
             <TextField
-                label="email"
+                label="Password"
+                name="password"
                 variant="outlined"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => {handleChange(e.target.name, e.target.value)}}
             />
             <FormControl fullWidth>
                 <InputLabel id="height-input-label">Height</InputLabel>
                 <Select
                     labelId="height-input-label"
                     id="height-select"
-                    onChange={(e) => setHeight(e.target.value)}
-                    value={height}
+                    onChange={(e) => {handleChange(e.target.name, e.target.value)}}
+                    value={state.height}
                 >
                     <MenuItem value={'Midget'}>Midget</MenuItem>
                     <MenuItem value={'Human'}>Human</MenuItem>
@@ -73,6 +103,13 @@ const RegisterPage = () => {
             </FormControl>
             <Button variant="contained" type="submit">Register</Button>
         </form>
+        <div className="log-in-wrapper">
+            <p>or</p>
+            <Button>
+                <Link to="/">Log in</Link>
+            </Button>
+            <p>if you already have an account</p>
+        </div>
     </div>;
 };
 
