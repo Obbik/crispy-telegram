b6454ce9546ea633515f478f735ffb6e8792a6f2
Katarzyna Mirowska <kmirowska@creatuity.com>
2022-02-28 15:22:06 +0100

    code refactor

--- frontend/src/utils/useFetch.js
+++ frontend/src/utils/useFetch.js
@@ -2,25 +2,21 @@ import React from 'react'
 import axios from 'axios';
 
 const useFetch = () => {
-const token = localStorage.getItem('userToken') || "";
+    const token = localStorage.getItem('userToken') || "";
 
-    const  query = (data, thenFunction, type ) =>   {
+    const query = (data, thenFunction, type) => {
         axios({
             method: type,
-            headers:{"Authorization" : token},
+            headers: {"Authorization": token},
             url: 'http://localhost:3001/',
             data: {
                 query: data
             }
-          }).then(thenFunction
-
-          ).catch(err => {
-              throw new err
-          });
+        }).then(thenFunction)
+            .catch(err => console.error(err));
     };
 
-
-  return { query}
+    return {query}
 }
 
 export default useFetch
