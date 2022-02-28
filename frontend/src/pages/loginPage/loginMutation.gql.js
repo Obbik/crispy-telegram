ee423bdc4037812a56f8fbbb240895596905142b
Katarzyna Mirowska <kmirowska@creatuity.com>
2022-02-28 14:53:23 +0100

    removed console logs

--- frontend/src/pages/loginPage/loginMutation.gql.js
+++
@@ -1,7 +0,0 @@
-
-export const LOGIN_MUTATION = (name, password, email) => `mutation  {
-    Login (name: "${name}", password: "${password}", email: "${email}"){
-      name,
-      token,
-    }
-  }`;
